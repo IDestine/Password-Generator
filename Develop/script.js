e74eb27618154e70843c80db80eb3d6f88430262
Isaiah Destine <zay@Isaiahs-MacBook-Pro-2.local>
2022-10-15 12:25:14 -0400

    Write password to the #password input, Set Password Length & Test values

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,27 @@
 // Assignment code here
 // Password Arrays
-const number= "0123456789";
-const specialCharacter= "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+const number = "0123456789";
+const specialCharacter = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 const alphaLower = "abcdefghijklmnopqrstuvwxyz"
 const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-function generatePasssword() {
+function generatePassword() {
 
   // Build Password using random characters
-  var newUserPassword = function(n, str){
-    var UserPassword = "":
-    for(var i=0; i<n; i++){
+  var newUserPassword = function (n, str) {
+    var UserPassword = "";
+    for (var i = 0; i < n; i++) {
       userPassword += str[Math.floor(Math.random() * str.length)];
-  }
-  return userPassword;
+    }
+    return userPassword;
   };
 
-  return newUserPassword;(SetPasswordLength(),BuildPasswordCharacterArray());
+  return newUserPassword; (SetPasswordLength(), BuildPasswordCharacterArray());
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
 
 // Write password to the #password input
 function writePassword() {
@@ -30,6 +31,27 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// Set Password Length & Test values
+function SetPasswordLength() {
+  var pwLength = 0;
+  var valid = false;
+
+  while (!valid) {
+    pwLength = parseInt(window.prompt("Enter Password Length (8-128 characters."));
+
+    if(pwLength >=8 && pwLength <= 128) {
+      valid = true;
+      return pwLength;
+  }
+  else{
+    window.alert("Password must be more than 8 characters and less than 128 characters")
+  }
+}
+
+}
+
+// Build available characters array 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
